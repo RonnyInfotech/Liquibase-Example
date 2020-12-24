82e9d8bc8a94ce85482f6624ae793df9a1e1da94
Bhautik <ronnyinfotech@gmail.com>
2020-12-24 15:17:58 +0530

    Liquibase Demo

---
+++ dbchangelog.sql
@@ -0,0 +1,22 @@
+-- --liquibase formatted sql  
+
+-- create table PERSON (
+--     ID int not null,
+--     FNAME varchar(100) not null
+-- );
+
+-- --changeset Raj:40 
+-- insert into demo (id, name) values (10, 'Deep');
+-- insert into demo (id,  name) values (20, 'Darshit');
+-- insert into demo (id,  name) values (30, 'Rahil');  
+
+-- -- DELETE Query
+-- --changeset Bhautik:1
+-- DELETE FROM demo WHERE id=10;
+-- DELETE FROM demo WHERE id=20;
+-- DELETE FROM demo WHERE id=30;
+
+-- -- DROP TABLE Query
+-- --changeset Bhautik:4
+-- DROP TABLE person;
+-- DROP TABLE test;
